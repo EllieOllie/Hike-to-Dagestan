24f5fbf07a49d203094e7c5d6a0533bb9c53268c
EllieOllie <82253017+EllieOllie@users.noreply.github.com>
2026-02-17 03:42:58 +0700

    add burger

--- js/main.js
+++ js/main.js
@@ -1,5 +1,31 @@
 window.addEventListener('DOMContentLoaded', () => {
 
+  function getBurger() {
+    const menu = document.body.querySelector('.burger-menu');
+    const button = document.body.querySelector('.burger-menu__btn', '.burger-menu__lines');
+    const links = document.body.querySelectorAll('.nav__list-link');
 
+    button.addEventListener('click', (e) => {
+      e.preventDefault();
+      toggleMenu();
+    });
+
+    links.forEach(link => link.addEventListener('click', () => toggleMenu()));
+
+    function toggleMenu() {
+      menu.classList.toggle('burger-menu--active');
+
+      if (menu.classList.contains('burger-menu--active')) {
+        menu.style.overflow = "hidden";
+        // document.body.style.overflow = "visible";
+
+      } else {
+        menu.style.overflow = "visible";
+        // document.body.style.overflow = "hidden";
+      }
+    }
+  }
+
+  getBurger();
 
 });
