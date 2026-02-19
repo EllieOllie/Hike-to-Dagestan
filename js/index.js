fe7777a1b83a30199d9a4097480159136fc48e57
EllieOllie <82253017+EllieOllie@users.noreply.github.com>
2026-02-20 04:43:33 +0700

    fix

--- js/index.js
+++ js/index.js
@@ -1,9 +1,13 @@
 import { navigate } from './navigate.js'
 import { btnUpScroll } from './btn-up.js'
+import { initAccordion } from './accordion.js';
 
 document.addEventListener('DOMContentLoaded', function () {
+
   // Запуск приложения
   navigate('home')
 
   btnUpScroll()
+
+  initAccordion()
 })
